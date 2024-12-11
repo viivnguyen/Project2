564c81b7403b4f966136f9d70b2261364cd5ef37
viivnguyen <vtnguyen@college.harvard.edu>
2024-12-11 15:51:49 -0500

    breh

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,5 +1,6 @@
 ## code to prepare `DATASET` dataset goes here
 
-dataset <- read.csv("C:/Users/Vivian N/Downloads/AY 24-25/Stat 108/Stat108Project2/countries.csv")
+# Import Dataset
+system("kaggle datasets download -d footprintnetwork/ecological-footprint --path data-raw --unzip")
 
 usethis::use_data(dataset, overwrite = TRUE)
