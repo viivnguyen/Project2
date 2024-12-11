6682497d5ddfbd8a8ef04006166c947a823091d4
viivnguyen <vtnguyen@college.harvard.edu>
2024-12-11 15:02:17 -0500

    more

---
+++ data-raw/DATASET.R
@@ -0,0 +1,5 @@
+## code to prepare `DATASET` dataset goes here
+
+dataset <- read.csv("C:/Users/Vivian N/Downloads/AY 24-25/Stat 108/Stat108Project2/countries.csv")
+
+usethis::use_data(dataset, overwrite = TRUE)
