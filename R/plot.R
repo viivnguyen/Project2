838ddacd859789ddbd82f8d10b57824c72620aaf
arielzwang <arielwang1@college.harvard.edu>
2024-12-12 21:30:40 -0500

    a working plot function? I think? with documentation

--- R/plot.R
+++ R/plot.R
@@ -1,30 +1,48 @@
-#' Title
+#' Scatter Plot Function
 #'
-#' @param data
-#' @param x
+#' This function generates a scatter plot using ggplot2, with customizable aesthetics and validation of input data types.
+#' It checks that the x and y variables are numeric and that the color variable is a factor.
 #'
-#' @return
+#' @param data A data frame containing the data to be plotted.
+#' @param x The name of the variable to be used on the x-axis (should be numeric).
+#' @param y The name of the variable to be used on the y-axis (should be numeric).
+#' @param color The name of the categorical variable to be used for color (should be a factor).
+#' @param ... Additional arguments to be passed to `ggplot2::geom_point()`.
+#'
+#' @return A `ggplot` object representing the scatter plot.
 #' @export
 #'
 #' @examples
+#' # Example usage of the scatter function
+#' data <- data.frame(x = rnorm(100), y = rnorm(100), color = factor(sample(c("A", "B"), 100, replace = TRUE)))
+#' scatter(data, x = "x", y = "y", color = "color")
+#'
+#' @import ggplot2
+#' @import dplyr
+#'
 
 # Function
-scatter <- function(data, x, y, color, alpha, ...){
-  #Determine if x is numeric
-  x_class <- data %>%
-    pull({{ x }}) %>%
-    is.numeric()
-  stopifnot(x_class)
-  y_class <- data %>%
-    pull({{ x }}) %>%
-    is.numeric()
-  stopifnot(y_class)
-  color_class <- data %>%
-    pull({{ color }}) %>%
-    is.factor()
-  stopifnot(color_class)
-  stopifnot(alpha >= 0 & alpha <= 1)
+scatter <- function(data, x, y, color, ...) {
 
-  ggplot(data = data, mapping = aes(x = {{ x }}, y = {{ y }}, color = {{ color }})) +
-    geom_point(alpha = alpha)
+  # Ensure x and y are numeric using base R
+  stopifnot(base::is.numeric(data[[x]]))   # Check if x is numeric
+  stopifnot(base::is.numeric(data[[y]]))   # Check if y is numeric
+
+  # Convert color to a factor if it is not already a factor
+  if (!base::is.factor(data[[color]])) {
+    data[[color]] <- base::factor(data[[color]])  # Convert to factor
+  }
+
+  stopifnot(base::is.factor(data[[color]])) # Check if color is a factor
+
+  # Check if the data has the expected number of rows
+  if (nrow(data) == 0) {
+    stop("The data contains no rows.")
+  }
+
+  # Plot the scatter plot
+  ggplot2::ggplot(data = data) +
+    ggplot2::aes(x = .data[[x]], y = .data[[y]], color = .data[[color]]) +  # Correctly map variables to aesthetics
+    ggplot2::geom_point(...)
 }
+
