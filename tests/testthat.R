f2d0fe65f9c44b8562767f636ea0237af50d2e46
viivnguyen <vtnguyen@college.harvard.edu>
2024-12-13 10:46:46 -0500

    error fix

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/articles/special-files.html
 
 library(testthat)
-library(Stat108Project2)
+library(asianamsurvey)
 
-test_check("Stat108Project2")
+test_check("asianamsurvey")
